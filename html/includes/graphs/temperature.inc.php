3544aaa896f94db1bdbaca7aef308b58e6a276ae
Krystian Paszkiewicz <krystian.paszkiewicz@intertec.pl>
2010-03-15 12:09:16 +0100

    added min temperature to graphs

--- html/includes/graphs/temperature.inc.php
+++ html/includes/graphs/temperature.inc.php
@@ -5,7 +5,7 @@ $scale_max = "40";
 
 include("common.inc.php");
 
-  $rrd_options .= " COMMENT:'                                 Last   Max\\n'";
+  $rrd_options .= " COMMENT:'                                 Min   Max\\n'";
 
   $sql = mysql_query("SELECT * FROM temperature where temp_id = '$temp'");
   $temperature = mysql_fetch_array(mysql_query("SELECT * FROM temperature where temp_id = '".mres($_GET['id'])."'"));
@@ -17,21 +17,14 @@ include("common.inc.php");
   $rrd_filename  = $config['rrd_dir'] . "/".$hostname."/" . safename("temp-" . $temperature['temp_descr'] . ".rrd");
 
   $rrd_options .= " DEF:temp=$rrd_filename:temp:AVERAGE";
-  $rrd_options .= " DEF:temp_max=$rrd_filename:temp:MAX";
-  $rrd_options .= " DEF:temp_min=$rrd_filename:temp:MIN";
-  $rrd_options .= " CDEF:tempwarm=temp_max,".$temperature['temp_limit'].",GT,temp,UNKN,IF";
-  $rrd_options .= " CDEF:tempcold=temp_min,20,LT,temp,UNKN,IF";
-  $rrd_options .= " AREA:temp_max#a5a5a5";
-  $rrd_options .= " AREA:temp_min#ffffffff";
-
-
-
-#  $rrd_options .= " AREA:temp#bbd392";
-#  $rrd_options .= " AREA:tempwarm#FFCCCC";
-#  $rrd_options .= " AREA:tempcold#CCCCFF";
-  $rrd_options .= " LINE1:temp#cc0000:'" . str_replace(':','\:',str_replace('\*','*',quotemeta($temperature['temp_descr_fixed'])))."'"; # Ugly hack :(
-  $rrd_options .= " LINE1:tempwarm#660000";
-  $rrd_options .= " GPRINT:temp:LAST:%3.0lfC";
+  $rrd_options .= " CDEF:tempwarm=temp,".$temperature['temp_limit'].",GT,temp,UNKN,IF";
+  $rrd_options .= " CDEF:tempcold=temp,20,LT,temp,UNKN,IF";
+  $rrd_options .= " AREA:temp#bbd392";
+  $rrd_options .= " AREA:tempwarm#FFCCCC";
+  $rrd_options .= " AREA:tempcold#CCCCFF";
+  $rrd_options .= " LINE1.5:temp#cc0000:'" . str_replace(':','\:',str_replace('\*','*',quotemeta($temperature['temp_descr_fixed'])))."'"; # Ugly hack :(
+  $rrd_options .= " LINE1.5:tempwarm#660000";
+  $rrd_options .= " GPRINT:temp:MIN:%3.0lfC";
   $rrd_options .= " GPRINT:temp:MAX:%3.0lfC\\\\l";
 
 ?>
